e9ff253841ab2715b622c117fbb86792230d2409
hurst <churst@uccs.edu>
2020-11-05 19:38:13 -0700

    Init base class for Alarm.py

--- Alarm.py
+++ Alarm.py
@@ -1,1 +1,62 @@
 "Create Alarm Class here for the CS3080 python final project."
+"""
+Private fields: 
+Name - gives the alarm name 
+Date - used for when the alarm is created format: (mm-dd-yyyy/hh:mm). Default is to use the current time + 1hr.
+reminderTime - another date used for when to send the email. Default is equal to the date; format:  (mm-dd-yyyy/hh:mm)
+Description - basic alarm description given by user
+Contact- email to send to when alarm ‘goes off’
+
+Public Fields:
+Getters and setters for all the above variables
+Constructor to initialize all the above private fields
+WriteValue() - returns  a string of the class variables comma delimited as follows: Name,Date,ReminderTime,Description,email
+SendToFile(FileLoc) - Writes the WriteValue() method’s value to the storage file (FileLoc).
+"""
+import re
+
+class Alarm:
+
+    def __init__(self, alarm_name = "", alarm_date = "", alarm_reminder = "", alarm_description = "", alarm_contact = ""):
+        self.__alarm_name = alarm_name
+        self.__alarm_date = alarm_date
+        self.__alarm_reminder = alarm_reminder
+        self.__alarm_description = alarm_description
+        self.__alarm_contact = alarm_contact
+
+    def get_alarm_name(self):
+        return self.__alarm_name
+    def get_alarm_date(self):
+        return self.__alarm_date
+    def get_alarm_reminder(self):
+        return self.__alarm_reminder
+    def get_alarm_description(self):
+        return self.__alarm_description
+    def get_alarm_contact(self):
+        return self.__alarm_contact
+
+    def set_alarm_name(self, name):
+        self.__alarm_name = name
+    def set_alarm_date(self, date):
+        self.__alarm_date = date
+    def set_alarm_reminder(self, reminder):
+        self.__alarm_reminder = reminder
+    def set_alarm_description(self, description):
+        self.__alarm_description = description
+    def set_alarm_contact(self, contact):
+        self.__alarm_contact = contact
+
+
+
+a1 = Alarm()
+
+a1.set_alarm_date(0)
+
+print(a1.get_alarm_date())
+
+
+
+
+
+
+
